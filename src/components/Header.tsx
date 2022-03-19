6be02bee51e8d76274b53864e5eb5bb71226a469
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-03-19 12:35:01 +0100

    creates sidebar, header and graphs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react"
-import { RiSearchLine } from "react-icons/ri"
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react"
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri"
 
 export function Header() {
   return (
@@ -50,6 +50,43 @@ export function Header() {
           fontSize="20"
           color="orange.500"
         />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize={20} />
+          <Icon as={RiUserAddLine} fontSize={20} />
+        </HStack>
+
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>
+              Paula Pompeo
+            </Text>
+            <Text color="gray.300" fontSize="small">
+              paulapompeoc@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Paula Pompeo"
+            src="https://avatars.githubusercontent.com/u/47497767?v=4"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,94 @@
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic"
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+// lazyloading -> sem passar na camada backend next
+// somente carrega pelo browser
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false,
+    },
+    stroke: {
+      width: 6
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  colors: [theme.colors.orange[500], ' #546E7A', '#E91E63'],
+  xaxis: {
+    type: 'datetime',
+    axisBordercolor: { color: theme.colors.orange[500] },
+    axisTicks: { color: theme.colors.orange[500] },
+    categories: [
+      '2022-03-18T00:00:00.000Z',
+      '2022-03-19T00:00:00.000Z',
+      '2022-03-20T00:00:00.000Z',
+      '2022-03-21T00:00:00.000Z',
+      '2022-03-22T00:00:00.000Z',
+      '2022-03-23T00:00:00.000Z',
+      '2022-03-24T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
+  }
+}
+
+const series = [
+  { name: "series1", data: [31, 120, 10, 28, 51, 18, 109] }
+]
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px">
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text>Week's subscription</Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          >
+            <Text>Conversion %</Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
   )
 }
