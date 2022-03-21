9234d92f7f6e30397c8fd1a0e2a161391302822e
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-03-21 17:01:26 +0100

    responsive sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,18 +1,24 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>
-          Paula Pompeo
-        </Text>
-        <Text color="gray.300" fontSize="small">
-          paulapompeoc@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>
+            Paula Pompeo
+          </Text>
+          <Text color="gray.300" fontSize="small">
+            paulapompeoc@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,29 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
+
+interface SidebarDrawerProviderProps {
+  children: ReactNode
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData)
+
+export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
+  // useDisclosure is a custom hook used to help handle common open, close, or toggle scenarios. It can be used to control feedback component such as Modal, AlertDialog, Drawer, etc.
+  const disclosure = useDisclosure()
+  const router = useRouter()
+
+  useEffect(() => {
+    disclosure.onClose()
+  }, [router.asPath])
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  )
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext)
