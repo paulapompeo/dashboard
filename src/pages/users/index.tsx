0bc20d386e90604f79fdf3d566471c77c69f865a
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-04-14 23:23:08 +0200

    adds react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,26 +12,48 @@ import {
   Thead,
   Tr,
   Text,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query"
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  // chave que a query sera armazenada no cache
+  // 1 param metodo para retornar os dados
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        ...user,
+        createdAt: new Date(user.createdAt).toLocaleDateString('en-US', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    })
+
+    return users
+  }, {
+    // milissegundos
+    staleTime: 1000 * 5 // 5 segundos
+  })
+
+  console.log({ data })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -57,97 +79,64 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="orange" />
-                </Th>
-                <Th>User</Th>
-                {isWideVersion && <Th>Registered at</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Error on loading data</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="orange" />
+                    </Th>
+                    <Th>User</Th>
+                    {isWideVersion && <Th>Registered at</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="orange" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Paula Pompeo</Text>
-                    <Text fontSize="sm" color="gray.300">paulapompeoc@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion ?? <Td>04 de Abril, 2022</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="orange"
-                    iconSpacing={isWideVersion ? '1.5' : '-0.5'}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
-                  >
-                    {isWideVersion && 'Edit'}
-                  </Button>
+                <Tbody>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="orange" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="orange"
+                            iconSpacing={isWideVersion ? '1.5' : '-0.5'}
+                            leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                          >
+                            {isWideVersion && 'Edit'}
+                          </Button>
 
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="orange" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Paula Pompeo</Text>
-                    <Text fontSize="sm" color="gray.300">paulapompeoc@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion ?? <Td>04 de Abril, 2022</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="orange"
-                    iconSpacing={isWideVersion ? '1.5' : '-0.5'}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
-                  >
-                    {isWideVersion && 'Edit'}
-                  </Button>
+                        </Td>
+                      </Tr>
+                    )
+                  })}
 
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="orange" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Paula Pompeo</Text>
-                    <Text fontSize="sm" color="gray.300">paulapompeoc@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion ?? <Td>04 de Abril, 2022</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="orange"
-                    iconSpacing={isWideVersion ? '1.5' : '-0.5'}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
-                  >
-                    {isWideVersion && 'Edit'}
-                  </Button>
-
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex >
     </Box >
